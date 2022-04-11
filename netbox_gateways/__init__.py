66d5539654ce3bd2e42622570a37068844b1c867
Jamie Murphy <jamiemurphyit@gmail.com>
2022-04-11 12:59:28 +0100

    adding manifest

--- netbox_gateways/__init__.py
+++ netbox_gateways/__init__.py
@@ -5,7 +5,7 @@ class NetBoxGatewayConfig(PluginConfig):
     name = "netbox_gateways"
     verbose_name = " Netbox Gateway"
     description = "Manage simple prefix default gateways"
-    version = "0.1"
+    version = "0.2"
     base_url = "nb_gateways"
 
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="netbox-gateways",
-    version="0.1",
+    version="0.2",
     description="Manage simple prefix default gateways",
     install_requires=[],
     packages=find_packages(),
