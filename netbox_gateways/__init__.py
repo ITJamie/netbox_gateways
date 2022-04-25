c448134d9745722e70e3eacd11beac3e7278220b
Jamie Murphy <jamiemurphyit@gmail.com>
2022-04-25 10:23:29 +0100

    0.4.3

--- netbox_gateways/__init__.py
+++ netbox_gateways/__init__.py
@@ -5,7 +5,7 @@ class NetBoxGatewayConfig(PluginConfig):
     name = "netbox_gateways"
     verbose_name = " Netbox Gateway"
     description = "Manage simple prefix default gateways"
-    version = "0.4.2"
+    version = "0.4.3"
     base_url = "nb-gateways"
 
 
