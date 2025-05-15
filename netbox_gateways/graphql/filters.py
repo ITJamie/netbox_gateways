85fa4fa80822c780acd1b4dccefa37f21f74f3d6
a084ed22 <90847254+a084ed22@users.noreply.github.com>
2025-05-15 11:36:46 +0200

    Remove deprecated autotype_decorator import and usage.

--- netbox_gateways/graphql/filters.py
+++ netbox_gateways/graphql/filters.py
@@ -1,5 +1,5 @@
 import strawberry_django
-from netbox.graphql.filter_mixins import autotype_decorator, BaseFilterMixin
+from netbox.graphql.filter_mixins import BaseFilterMixin
 
 from netbox_gateways.models import Gateway
 from netbox_gateways.filtersets import GatewayFilterSet
@@ -10,6 +10,5 @@ __all__ = (
 
 
 @strawberry_django.filter(Gateway, lookups=True)
-@autotype_decorator(GatewayFilterSet)
 class GatewayFilter(BaseFilterMixin):
     pass
