f4c0e49cce0c90e6343c624b81612a7176087389
Jamie (Bear) Murphy <git@jam.ie>
2026-01-20 20:30:48 +0000

    add docs for future uploads

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+README = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="netbox-gateways",
     version="0.9.0",
     description="Manage simple prefix default gateways",
+    long_description=README,
+    long_description_content_type="text/markdown",
     install_requires=[],
     packages=find_packages(),
     include_package_data=True,
